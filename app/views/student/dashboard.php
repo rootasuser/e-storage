6da57b382e37835de3fa572843164ca0c0c474b1
Windel Navales <windelvlog@gmail.com>
2025-04-02 19:10:35 +0800

    update

--- app/views/student/dashboard.php
+++ app/views/student/dashboard.php
@@ -47,121 +47,63 @@ $profileImage = empty($user['profile_pic'])
 
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
-
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="description" content="">
     <meta name="author" content="devwin">
-
     <title>Dashboard</title>
-
-    <!-- Custom fonts for this template-->
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" rel="stylesheet" type="text/css">
     <link
         href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
         rel="stylesheet">
-
-    <!-- Custom styles for this template-->
     <link href="../../../public/assets/css/m-windev.css" rel="stylesheet">
-
 </head>
-
 <body id="page-top">
-
-    <!-- Page Wrapper -->
     <div id="wrapper">
-
-        <!-- Sidebar -->
         <ul class="navbar-nav sidebar sidebar-dark accordion" id="accordionSidebar" style="background-color: #00296b;">
-
-            <!-- Sidebar - Brand -->
             <a class="sidebar-brand d-flex align-items-center justify-content-center" href="?page=analytics">
-   
                 <div class="sidebar-brand-text mx-3"><sup>STUDENT PANEL</sup></div>
             </a>
-
-            <!-- Divider -->
             <hr class="sidebar-divider my-0">
-
-            <!-- Nav Item - Dashboard -->
             <li class="nav-item active">
                 <a class="nav-link" href="?page=analytics">
                     <i class="fas fa-fw fa-tachometer-alt"></i>
-                    <span>Dashboard</span></a>
+                    <span id="dashboard"></span></a>
             </li>
-
-            <!-- Divider -->
             <hr class="sidebar-divider">
-
-           
-            <!-- Heading -->
             <div class="sidebar-heading">
-                Data Management
+                <span id="data-management"></span>
             </div>
-
-          
-
-            <!-- Nav Item -  -->
             <li class="nav-item">
                 <a class="nav-link" href="?page=browse">
                     <i class="fas fa-fw fa-globe"></i>
-                    <span>Browse Research</span></a>
+                    <span id="browse"></span></a>
             </li>
-
-            <!-- Nav Item -  -->
             <li class="nav-item">
                 <a class="nav-link" href="?page=requests">
                     <i class="fas fa-fw fa-code-pull-request"></i>
-                    <span>Requests</span></a>
+                    <span id="requests"></span></a>
             </li>
-
- <!-- Nav Item -  -->
- <li class="nav-item">
+            <li class="nav-item">
                 <a class="nav-link" href="?page=bookmarks">
                     <i class="fas fa-fw fa-bookmark"></i>
-                    <span>Bookmarks</span></a>
+                    <span id="bookmarks"></span></a>
             </li>
-
-            <!-- Divider -->
             <hr class="sidebar-divider d-none d-md-block">
-
-            <!-- Sidebar Toggler (Sidebar) -->
             <div class="text-center d-none d-md-inline">
                 <button class="rounded-circle border-0" id="sidebarToggle"></button>
             </div>
-
-        
-
         </ul>
-        <!-- End of Sidebar -->
-
-        <!-- Content Wrapper -->
         <div id="content-wrapper" class="d-flex flex-column">
-
-            <!-- Main Content -->
             <div id="content">
-
-                <!-- Topbar -->
                 <nav class="navbar navbar-expand navbar-light bg-white topbar mb-4 static-top shadow">
-
-                    <!-- Sidebar Toggle (Topbar) -->
                     <button id="sidebarToggleTop" class="btn btn-link d-md-none rounded-circle mr-3">
                         <i class="fa fa-bars"></i>
                     </button>
-
-                  
-                    <!-- Topbar Navbar -->
                     <ul class="navbar-nav ml-auto">
-
-                       
-                     
-
                         <div class="topbar-divider d-none d-sm-block"></div>
-
-                        <!-- Nav Item - User Information -->
                         <li class="nav-item dropdown no-arrow">
                             <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button"
                                 data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
@@ -169,58 +111,36 @@ $profileImage = empty($user['profile_pic'])
                                 <img class="img-profile rounded-circle"
                                     src="<?= $user['profile_pic'] ? 'data:image/jpeg;base64,' . base64_encode($user['profile_pic']) : $profileImage ?>">
                             </a>
-                            <!-- Dropdown - User Information -->
                             <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in"
                                 aria-labelledby="userDropdown">
                                 <a class="dropdown-item" href="?page=profile">
                                     <i class="fas fa-user fa-sm fa-fw mr-2 text-gray-400"></i>
-                                    Profile
+                                  <span id="profile"></span>
                                 </a>
                                 <div class="dropdown-divider"></div>
                                 <a class="dropdown-item" href="/e-storage/public/logout" data-toggle="modal" data-target="#logoutModal">
                                     <i class="fas fa-sign-out-alt fa-sm fa-fw mr-2 text-gray-400"></i>
-                                    Logout
+                                    <span id="logoutSpan"></span>
                                 </a>
                             </div>
                         </li>
-
                     </ul>
-
                 </nav>
-                <!-- End of Topbar -->
-
-                <!-- Begin Page Content -->
                 <div class="container-fluid">
-             
-
-                    <?php
+            <?php
             $page = filter_input(INPUT_GET, 'page', FILTER_SANITIZE_SPECIAL_CHARS) ?: 'analytics';
             $allowedPages = ['analytics', 'browse', 'requests', 'bookmarks', 'profile', 'logs', 'users', 'account'];
             if (!in_array($page, $allowedPages, true)) { $page = '404'; }
             $viewFile = __DIR__ . '/templates/' . $page . '.php';
             if (is_readable($viewFile)) { include $viewFile; } else { http_response_code(404); echo '<h2>404 - Page Not Found</h2>'; }
-        ?>
-            
+                ?>         
                 </div>
-                <!-- /.container-fluid -->
-
             </div>
-            <!-- End of Main Content -->
-
-         
-
         </div>
-        <!-- End of Content Wrapper -->
-
     </div>
-    <!-- End of Page Wrapper -->
-
-    <!-- Scroll to Top Button-->
     <a class="scroll-to-top rounded" href="#page-top">
         <i class="fas fa-angle-up"></i>
     </a>
-
-    <!-- Logout Modal-->
     <div class="modal fade" id="logoutModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
         aria-hidden="true">
         <div class="modal-dialog" role="document">
@@ -240,14 +160,11 @@ $profileImage = empty($user['profile_pic'])
         </div>
     </div>
 
-    <!-- Bootstrap core JavaScript-->
+
     <script src="../../../public/assets/jQuery/jquery.min.js"></script>
     <script src="../../../public/assets/bootstrap/bootstrap.bundle.min.js"></script>
-
-    <!-- Core plugin JavaScript-->
+    <script src="../../../public/assets/bootstrap/bootstrap.bundles.min.js"></script>
     <script src="../../../public/assets/jquery-easing/jquery.easing.min.js"></script>
-
-    <!-- Custom scripts for all pages-->
     <script src="../../../public/assets/js/m-windev.js"></script>
 
 
